fd6c86963b1cee209c4afe2ee954c5c3d39fd801
Aurian Lira <aurianlira972@gmail.com>
2024-05-10 11:53:34 -0300

    implementa segmented control, lógica dos botoes e espacamento

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -13,8 +13,12 @@ struct ContentView: View {
     @State var months: Int?
     @State var result: Int?
     
+    let portes = ["Pequeno", "Médio", "Grande"]
+    @State var porteSelecionado = "Pequeno"
+    
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 20.0) {
+//            Spacer()
             Text("Qual a idade do seu cão?")
             Text("Anos:")
             TextField(
@@ -32,21 +36,33 @@ struct ContentView: View {
             )
 //            .textFieldStyle(.roundedBorder)
 //            .keyboardType(.numberPad)
+//            Spacer()
             Text("Porte:")
+            
+            Picker("Portes", selection: $porteSelecionado) {
+                ForEach(portes, id:\.self) { porte
+                    in Text(porte)
+                }
+            }.pickerStyle(.segmented)
+            
+            Divider()
+            Spacer()
+            
             if let result {
                 Text("Seu cachorro tem, em idade humana...")
                 Text("\(result) anos")
             } else {
                 Image(ImageResource.clarinha)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 150)
+                .resizable()
+                .scaledToFit()
+                .frame(maxHeight: 150)
                 .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .shadow(radius: 20)
             }
-            Button("Cãocular") {
-                print("Cãocular")
-                result = 23
-            }
+            
+            Spacer()
+            
+            Button("Cãocular", action: processYears)
             .frame(maxWidth: .infinity)
             .frame(maxHeight: 50)
             .background(.indigo)
@@ -57,6 +73,31 @@ struct ContentView: View {
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
         .padding()
+    }
+    
+    func processYears() {
+        print("Cãocular")
+        guard let years, let months else {
+            print("Campos não preenchidos")
+            return
+        }
+        guard months > 0 || years > 0 else {
+            print("Ao menos um campo deve ser maior que zero")
+            return
+        }
+    
+        var multiplicador: Int = 0
+        switch porteSelecionado {
+        case "Pequeno":
+            multiplicador = 6
+        case "Médio":
+            multiplicador = 7
+        case "Grande":
+            multiplicador = 8
+        default:
+            multiplicador = 0
+        }
+        result = years * multiplicador + months * multiplicador/12
     }
 }
 
