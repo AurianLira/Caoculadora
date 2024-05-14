d2715474b37011e497d7bfa79fc9a45639972de1
Aurian Lira <aurianlira972@gmail.com>
2024-05-14 11:02:24 -0300

    Adiciona navbar e scrollview

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -15,68 +15,81 @@ struct ContentView: View {
     @State var porteSelected = Porte.pequeno
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20.0) {
-            //            Spacer()
-            Text("Qual a idade do seu cão?")
-                .font(.header5)
-            Text("Anos:")
-                .font(.body1)
-            TextField(
-                "Quantos anos completos tem o seu cão?",
-                value: $years,
-                format: .number
-            )
-            
-            Text("Meses:")
-                .font(.body1)
-            TextField(
-                "E quanto meses além disso ele tem?",
-                value: $months,
-                format: .number
-            )
-            
-            Text("Porte:")
-                .font(.body1)
-            
-            Picker("Portes", selection: $porteSelected) {
-                ForEach(Porte.allCases, id:\.self) { porte
-                    in Text(porte.rawValue)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20.0) {
+                    Text("Qual a idade do seu cão?")
+                        .font(.header5)
+                    Text("Anos:")
+                        .font(.body1)
+                    TextField(
+                        "Quantos anos completos tem o seu cão?",
+                        value: $years,
+                        format: .number
+                    )
+                    
+                    Text("Meses:")
+                        .font(.body1)
+                    TextField(
+                        "E quanto meses além disso ele tem?",
+                        value: $months,
+                        format: .number
+                    )
+                    
+                    Text("Porte:")
+                        .font(.body1)
+                    
+                    Picker("Portes", selection: $porteSelected) {
+                        ForEach(Porte.allCases, id:\.self) { porte
+                            in Text(porte.rawValue)
+                        }
+                    }.pickerStyle(.segmented)
+                    
+                    Divider()
+                    Spacer()
+                    
+                    if let result {
+                        Text("Seu cachorro tem, em idade humana...")
+                            .font(.body1)
+                        Text("\(result) anos")
+                            .font(.display)
+                    } else {
+                        Image(ImageResource.clarinha)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxHeight: 150)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            .shadow(radius: 20)
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Cãocular", action: processYears)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(.indigo)
+                        .foregroundStyle(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .bold()
+                        .font(.body1)
                 }
-            }.pickerStyle(.segmented)
-            
-            Divider()
-            Spacer()
-            
-            if let result {
-                Text("Seu cachorro tem, em idade humana...")
-                    .font(.body1)
-                Text("\(result) anos")
-                    .font(.display)
-            } else {
-                Image(ImageResource.clarinha)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 150)
-                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                    .shadow(radius: 20)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .padding()
+                .containerRelativeFrame(.vertical)
             }
-            
-            Spacer()
-            
-            Button("Cãocular", action: processYears)
-                .frame(maxWidth: .infinity)
-                .frame(maxHeight: 50)
-                .background(.indigo)
-                .foregroundStyle(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .bold()
-                .font(.body1)
+            .navigationTitle("Cãoculadora")
+            .scrollDismissesKeyboard(.immediately)
+            .toolbarBackground(.indigo, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbarColorScheme(.dark, for: .navigationBar)
         }
-        .textFieldStyle(.roundedBorder)
-        .keyboardType(.numberPad)
-        .padding()
+        .fontDesign(.rounded)
     }
-    
+}
+
+//MARK: -Function
+extension ContentView {
     func processYears() {
         print("Cãocular")
         guard let years, let months else {
